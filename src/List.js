20bdcb9dd85f6854891c5b6c846e2a7f4b2c0968
PhongTran1804 <trannguyenphongg@gmail.com>
2023-10-31 20:33:52 +0700

    update the load indicator

--- src/List.js
+++ src/List.js
@@ -1,9 +1,9 @@
-import { React, useEffect, useRef} from 'react';
+import { React, useEffect, useRef, useState} from 'react';
 import { useQueryClient , useInfiniteQuery } from 'react-query';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 const fetchItems = async ( key ) => {
-  console.log(key);
+  // console.log(key);
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${key.searchText}&page=${key.pageParam}&per_page=20&client_id=DI6uN2nH-s1lI6pzCVhUCBz7uoh4-438L3XJ0jh6JGw`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
@@ -13,6 +13,7 @@ const fetchItems = async ( key ) => {
 
 function List({searchText}) {
   const queryClient = useQueryClient();
+  const [loadingVisible, setLoadingVisible] = useState(false);
   const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery(
     searchText,
     ({ pageParam = 1 }) => fetchItems({ searchText, pageParam}),
@@ -36,10 +37,10 @@ function List({searchText}) {
     }
   };
 
-   // Clear the query cache when searchText changes
+   // Clear the query cache if searchText changes
    useEffect(() => {
     queryClient.removeQueries([searchText]);
-    // Auto load the first page when the searchText changes
+    // Load the first page when the searchText changes
     fetchNextPage();
   }, [searchText, queryClient, fetchNextPage]);
 
@@ -66,6 +67,15 @@ function List({searchText}) {
     };
   }, [infiniteScrollRef]);
 
+  useEffect(() => {
+    // console.log(hasNextPage);
+    if (searchText === '') {
+      setLoadingVisible(false);
+    } else {
+      setLoadingVisible(true);
+    }
+  }, [hasNextPage]);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -80,7 +90,7 @@ function List({searchText}) {
         dataLength={allItems.length}
         next={loadMore}
         hasMore={hasNextPage}
-        loader={<h4>Loading...</h4>}
+        loader={loadingVisible ? <h4>Loading...</h4> : null}
         ref={infiniteScrollRef}
       >
         {allItems.map(item => (
